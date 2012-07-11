5f4d5fdc6568804bd6d5e1a0c3f7e3bd572fec1d
Clive Baker <clive@clivebaker.com>
2012-07-11 12:16:59 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SharingAddthis::Application.config.secret_token = '4604a73105edd1630307ee5482ba4db240dffeffdff36ea034215c440df063e168570e9bfa306f17ee822bd469ee79582d1fa75ccb1a75ed83186f29e359f14a'
